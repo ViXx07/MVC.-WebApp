ea5ecd04ba46901508ec9bc858a6bb5605f09166
ViXx07 <vicen.perezb@duocuc.cl>
2024-11-30 20:25:54 -0300

    Delete y Update

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -21,7 +21,80 @@ namespace WebApplication.Controllers
 
         public IActionResult Create()
         {
+
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Item obj)
+        {
+            if(obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The name cannot be the same as the desciption.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Items");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Item? itemFromDB = _context.Items.Find(id);
+
+            if(itemFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(itemFromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Item obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "The name cannot be the same as the description.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Items.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Items");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Item? itemFromDB = _context.Items.Find(id);
+
+            if (itemFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(itemFromDB);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            Item? itemFromDB = _context.Items.Find(id);
+            if (itemFromDB == null)
+            {
+                return NotFound();
+            }
+            _context.Items.Remove(itemFromDB);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Items");
+            }
     }
 }
--- Models/Item.cs
+++ Models/Item.cs
@@ -1,11 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace WebApplication.Models
 {
     public class Item
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Item Name")]
+        [MinLength(1)]
         public required string Name { get; set; }
+        [MinLength(1)] [MaxLength(50)]
         public string? Description { get; set; }
 
     }
